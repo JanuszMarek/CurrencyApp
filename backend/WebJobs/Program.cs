17c70d3977daa30e1764989c8fa32ee0283319fd
Marek <janusz.marek@corp.altegrity.com>
2021-03-06 23:20:01 +0100

    setup basic WebJobs

--- backend/WebJobs/Program.cs
+++ backend/WebJobs/Program.cs
@@ -1,45 +1,64 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace WebJobs
 {
     class Program
     {
-        private static readonly string EnvironmentName = Environment.GetEnvironmentVariable("ENVIRONMENT");
-        private static bool IsDevelopment => EnvironmentName == "Development";
+        private static readonly string CurrentEnvironment = Environment.GetEnvironmentVariable("ENVIRONMENT");
+        private static bool IsDevelopment => CurrentEnvironment == EnvironmentName.Development;
         private static IConfiguration configuration;
 
-        static void Main(string[] args)
+        static async Task Main()
         {
-            BuildConfiguration();
+            configuration = BuildConfiguration();
+            var host = BuildHost();
 
-            var builder = new HostBuilder()
-                .ConfigureWebJobs(webJobConfiguration =>
-                {
-                    webJobConfiguration.AddAzureStorageCoreServices();
-                    webJobConfiguration.AddTimers();
-                })
-                .ConfigureServices(serviceCollection => serviceCollection.AddTransient<SayHelloWebJob>())
-                .Build();
-
-            builder.Run();
+            using (host)
+            {
+                await host.RunAsync();
+            }
         }
 
-        private static void BuildConfiguration()
+        private static IConfigurationRoot BuildConfiguration()
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
             if (IsDevelopment)
             {
-                builder.AddJsonFile($"appsettings.{EnvironmentName}.json", optional: true, reloadOnChange: true);
+                builder.AddJsonFile($"appsettings.{CurrentEnvironment}.json", optional: true);
             }
 
-            configuration = builder.AddEnvironmentVariables().Build();
+            return builder.AddEnvironmentVariables().Build();
+        }
+
+        private static IHost BuildHost()
+        {
+            var builder = new HostBuilder();
+
+            if (IsDevelopment)
+            {
+                builder.UseEnvironment(EnvironmentName.Development);
+            }
+
+            builder.ConfigureWebJobs(webJobConfiguration =>
+                    {
+                        webJobConfiguration.AddAzureStorageCoreServices().AddTimers();
+                    })
+                .ConfigureLogging((context, b) =>
+                {
+                    b.AddConsole();
+                })
+                .ConfigureServices(serviceCollection => serviceCollection.AddTransient<WebJobTimer>())
+                .UseConsoleLifetime();
+
+            return builder.Build();
         }
     }
 }
--- backend/WebJobs/WebJobTimer.cs
+++ backend/WebJobs/WebJobTimer.cs
@@ -1,17 +1,13 @@
 ﻿using Microsoft.Azure.WebJobs;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebJobs
 {
-    public class SayHelloWebJob
+    public class WebJobTimer
     {
         //If I scale out, it should still only have one instance running
         [Singleton] 
-        public static void TimerTick([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        public static void CryptoApiTimer([TimerTrigger("0/1 * * * *")] TimerInfo myTimer)
         {
             Console.WriteLine($"Hello at {DateTime.UtcNow.ToString()}");
         }
