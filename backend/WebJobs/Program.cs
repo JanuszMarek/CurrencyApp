0034e0ecae61d95e5f77a328ffc9f9622429fc38
Marek <janusz.marek@corp.altegrity.com>
2021-03-06 18:27:32 +0100

    Add WebJobs project

---
+++ backend/WebJobs/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+
+namespace WebJobs
+{
+    class Program
+    {
+        private static readonly string EnvironmentName = Environment.GetEnvironmentVariable("ENVIRONMENT");
+        private static bool IsDevelopment => EnvironmentName == "Development";
+        private static IConfiguration configuration;
+
+        static void Main(string[] args)
+        {
+            BuildConfiguration();
+
+            var builder = new HostBuilder()
+                .ConfigureWebJobs(webJobConfiguration =>
+                {
+                    webJobConfiguration.AddAzureStorageCoreServices();
+                    webJobConfiguration.AddTimers();
+                })
+                .ConfigureServices(serviceCollection => serviceCollection.AddTransient<SayHelloWebJob>())
+                .Build();
+
+            builder.Run();
+        }
+
+        private static void BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            if (IsDevelopment)
+            {
+                builder.AddJsonFile($"appsettings.{EnvironmentName}.json", optional: true, reloadOnChange: true);
+            }
+
+            configuration = builder.AddEnvironmentVariables().Build();
+        }
+    }
+}
---
+++ backend/WebJobs/SayHelloWebJob.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebJobs
+{
+    public class SayHelloWebJob
+    {
+        //If I scale out, it should still only have one instance running
+        [Singleton] 
+        public static void TimerTick([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        {
+            Console.WriteLine($"Hello at {DateTime.UtcNow.ToString()}");
+        }
+    }
+}
