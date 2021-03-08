c16b6316bc4dde186aef33df99fa044c42e8e1af
Marek <janusz.marek@corp.altegrity.com>
2021-03-08 22:38:07 +0100

    Add Context

---
+++ backend/EF/CurrencyContext.cs
@@ -0,0 +1,17 @@
+﻿using EF.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF
+{
+    public class CurrencyContext : DbContext
+    {
+        public virtual DbSet<Symbol> Symbols { get; set; }
+        public virtual DbSet<SymbolType> SymbolTypes { get; set; }
+        public virtual DbSet<CurrencyPrice> CurrencyPrices { get; set; }
+        public virtual DbSet<CurrencyDelta> CurrencyDeltas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
