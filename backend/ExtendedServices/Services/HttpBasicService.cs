8a6fea73b29fa1a3373e95563c3c545016165693
Marek <janusz.marek@corp.altegrity.com>
2021-03-08 21:08:43 +0100

    Create httpClient services

---
+++ backend/ExtendedServices/Services/HttpBasicService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ExtendedServices.Services
+{
+    public abstract class HttpBasicService
+    {
+        private readonly HttpClient httpClient;
+
+        public HttpBasicService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        protected async Task<T> GetAsync<T>(string url)
+        {
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<T>();
+            }
+
+            throw new Exception($"{nameof(GetAsync)}: {response.StatusCode}, {response.ReasonPhrase}");
+        }
+    }
+}
---
+++ backend/ExtendedServices/Services/HttpCoinlibService.cs
@@ -0,0 +1,34 @@
+﻿using ExtendedServices.Models.CoinLib;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+
+namespace ExtendedServices.Services
+{
+    public interface IHttpCoinlibService
+    {
+        Task<CoinModel> GetCoinsAsync(IEnumerable<string> cryptoSymbols, string priceCurrency);
+    }
+
+    public class HttpCoinlibService : HttpBasicService, IHttpCoinlibService
+    {
+        public HttpCoinlibService(HttpClient httpClient) : base(httpClient)
+        {
+        }
+
+        public async Task<CoinModel> GetCoinsAsync(IEnumerable<string> cryptoSymbols, string priceCurrency)
+        {
+            var parameters = new Dictionary<string, string>
+            {
+                ["symbol"] = string.Join(",", cryptoSymbols),
+                ["pref"] = priceCurrency
+            };
+
+            var query = new FormDataCollection(parameters).ReadAsNameValueCollection().ToString();
+            var response = await GetAsync<CoinModel>(query);
+
+            return response;
+        }
+    }
+}
