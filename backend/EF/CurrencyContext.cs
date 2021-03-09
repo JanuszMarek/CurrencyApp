7e7dba321ba1099d3e41b2b3a5fce5396fa18923
Marek <janusz.marek@corp.altegrity.com>
2021-03-09 20:58:41 +0100

    Setup EF and Context, add ConnectionString to Database

--- backend/EF/CurrencyContext.cs
+++ backend/EF/CurrencyContext.cs
@@ -5,13 +5,17 @@ namespace EF
 {
     public class CurrencyContext : DbContext
     {
+        private readonly string _connectionString;
+
+        public CurrencyContext(DbContextOptions<CurrencyContext> options)
+            : base(options)
+        {
+
+        }
+
         public virtual DbSet<Symbol> Symbols { get; set; }
         public virtual DbSet<SymbolType> SymbolTypes { get; set; }
         public virtual DbSet<CurrencyPrice> CurrencyPrices { get; set; }
         public virtual DbSet<CurrencyDelta> CurrencyDeltas { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-        }
     }
 }
