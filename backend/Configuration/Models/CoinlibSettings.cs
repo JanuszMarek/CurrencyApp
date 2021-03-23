181368e10e58b5d8a5f54a28089e8700941ce449
Marek <janusz.marek@corp.altegrity.com>
2021-03-23 21:59:09 +0100

    Create Configuration lib to store Configuration Models

---
+++ backend/Configuration/Models/CoinlibSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Configuration.Models
+{
+    public class CoinlibSettings
+    {
+        public string PriceCurrency { get; set; }
+        public string[] Symbols { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
