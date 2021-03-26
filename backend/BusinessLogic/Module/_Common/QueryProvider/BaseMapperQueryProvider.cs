81ef66dfebbb5195e4cab7850075e63df02e5f3d
Marek <janusz.marek@corp.altegrity.com>
2021-03-26 23:15:13 +0100

    New BaseMapperQueryProvider

---
+++ backend/BusinessLogic/Module/_Common/QueryProvider/BaseMapperQueryProvider.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Entity.Context;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace BusinessLogic.Module._Common.QueryProvider
+{
+    public interface IBaseMapperQueryProvider<T>: IBaseQueryProvider<T> where T : class
+    {
+    }
+
+    public class BaseMapperQueryProvider<T> : BaseQueryProvider<T> where T : class
+    {
+        protected readonly IMapper mapper;
+
+        public BaseMapperQueryProvider(CurrencyContext context, IMapper mapper) : base(context)
+        {
+            this.mapper = mapper;
+        }
+
+        protected IQueryable<TDest> GetQuery<TDest>(Expression<Func<T, bool>> wherePredict, bool asNoTracking = false, Expression<Func<T, bool>> orderPredict = null, bool orderDesc = false) where TDest: class
+        {
+            return GetQuery(wherePredict, asNoTracking, orderPredict, orderDesc).ProjectTo<TDest>(mapper.ConfigurationProvider);
+        }
+    }
+}
